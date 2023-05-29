6b7e74357935ad7e54f63ab517041241e820175a
Angel9921 <la.angarita@uniandes.edu.co>
2023-05-29 12:58:22 -0500

    error

--- historiaclinicas/historiaclinicas/urls.py
+++ historiaclinicas/historiaclinicas/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
 
 from . import views
-from .views import historiaclinica_update, historiaclinica_list, historiaclinica_create
+from .views import historiaclinica_list, historiaclinica_create
 urlpatterns = [
     path('historiaclinicas/', views.historiaclinica_list, name='historiaclinicas'),
     path('historiaclinicacreate/', csrf_exempt(views.historiaclinica_create), name='historiaclinicaCreate'),
